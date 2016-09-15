a99d85f02fe4ff3306f16f956866d926d4c9a3d0
hs-nazuna <h-suzuki@ist.hokudai.ac.jp>
2016-09-15 14:55:05 +0900

    add sample.zdd

--- main.cpp
+++ main.cpp
@@ -7,5 +7,15 @@ using namespace hybriddd;
 int main(int argc, char* argv[]) {
 	MyTdZdd mytdzdd(argv[1], "as-is");
 	MyEval result = mytdzdd.S_T_Path(0, 4, true);
+	
+	int I = mytdzdd.getGraph().getNumOfI();
+	for (int i = 0; i < I; ++i) {
+		HybridGraph::Item item = mytdzdd.getGraph().getItemAf(i);
+		std::cout << "Lev." << I - i << " : ";
+		if (item.isvertex) std::cout << "v" << item.v << std::endl;
+		else std::cout << "e={v" << item.v1 << ", v" << item.v2 << "}" << std::endl;
+	}
+	
 	result.dump(std::cout);
+	result.dumpSapporo("sample.zdd");
 }
